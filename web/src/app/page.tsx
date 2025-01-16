067502113fd11af248a8681db6f0d7fac98db914
Utkarsh Mishra <utmishra@gmail.com>
2025-01-16 14:13:56 +0100

    feat: Theme Switcher, updated font, favicon & fixed hydration issues

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,32 +1,38 @@
 import Head from '@components/ui/homepage/head'
 import Links from '@components/ui/homepage/links'
 import Search from '@components/ui/homepage/search'
+import { ThemeSwitcher } from '@components/ui/homepage/theme-switcher'
 
 import { Flex, Card } from '@radix-ui/themes'
 
 export default function Home() {
   return (
-    <Flex
-      align="center"
-      direction="column"
-      justify="center"
-      gap="2"
-      width="100vw"
-      height="100vh"
-      p="2"
-    >
-      <Card
-        size={{
-          xl: '5',
-          md: '4',
-          sm: '3',
-          xs: '1',
-        }}
+    <>
+      <Flex direction="row-reverse" p="3">
+        <ThemeSwitcher />
+      </Flex>
+      <Flex
+        align="center"
+        direction="column"
+        justify="center"
+        gap="2"
+        width="100vw"
+        height="calc(100vh - 40px)"
+        p="2"
       >
-        <Head />
-        <Search />
-        <Links />
-      </Card>
-    </Flex>
+        <Card
+          size={{
+            xl: '5',
+            md: '4',
+            sm: '3',
+            xs: '1',
+          }}
+        >
+          <Head />
+          <Search />
+          <Links />
+        </Card>
+      </Flex>
+    </>
   )
 }
--- web/src/ui/homepage/search.tsx
+++ web/src/ui/homepage/search.tsx
@@ -3,7 +3,7 @@ import { Box, TextField } from '@radix-ui/themes'
 
 export default function Search() {
   return (
-    <Box width="400px">
+    <Box>
       <TextField.Root placeholder="Ask about me…" size="3">
         <TextField.Slot>
           <MagnifyingGlassIcon height="16" width="16" />
