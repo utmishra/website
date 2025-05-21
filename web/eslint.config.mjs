3e5aebded98ea49e925fda64cd1d439f44606f3a
Utkarsh Mishra <utmishra@gmail.com>
2025-05-21 17:33:19 +0200

    refactor: Unused imports

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -1,7 +1,6 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
-import reactCompiler from 'eslint-plugin-react-compiler'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
